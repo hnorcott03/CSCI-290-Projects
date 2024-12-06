87100ad58a93db8228491183785040daf15474ae
hnorcott03 <harlandbnorcott@smccme.edu>
2024-12-05 21:53:22 -0500

    update phraseranking

--- Part09-Harland-Sam-Brady-Bill/src/student/PhraseRanking.java
+++ Part09-Harland-Sam-Brady-Bill/src/student/PhraseRanking.java
@@ -1,49 +1,72 @@
 package student;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.regex.*;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author harlandnorcott
  */
 public class PhraseRanking {
-    
+
     static int rankPhrase(String lyrics, String lyricsPhrase) {
-        
-        lyrics = lyrics.toLowerCase().replaceAll("[^a-z ]", " ");
-        lyricsPhrase = lyricsPhrase.toLowerCase().replaceAll("[^a-z ]", " ");
-        String[] searchWords = lyricsPhrase.split("\\s+");
-        String[] lyricsWords = lyrics.split("\\s+");
-        
-        int rank = 0;
-        
-        // Iterate through lyrics array until a word matches the first
-        // the first word of the search phrase.
-        
-        int[] firstWordMatchLocs = new int[30];
-        int j = 0;
-        
-        for(int i = 0; i < lyricsWords.length; i++) {
-            firstWordMatchLocs[j] = i;
-            j++;
+
+        Pattern phrasePattern = Pattern.compile("[a-zA-z_0-9]+", Pattern.CASE_INSENSITIVE);
+        Matcher phraseMatcher = phrasePattern.matcher(lyricsPhrase);
+        List<String> phraseWords = new ArrayList<>();
+
+        while (phraseMatcher.find()) {
+            phraseWords.add(phraseMatcher.group());
         }
-        
-        StringBuilder fullLyricPhrase = new StringBuilder();
-        
-        for(int k = 0; k < firstWordMatchLocs.length; k++) {
-            int l = firstWordMatchLocs[k];
-            while(!lyricsWords[l].equals(searchWords[searchWords.length - 1])) {
-                fullLyricPhrase.append(" ").append(lyricsWords[l]);
-                l++;
+
+        List<List<Integer>> matchers = new ArrayList<>();
+        for (String word : phraseWords) {
+            Pattern wordPattern = Pattern.compile("\\b" + word + "\\b", Pattern.CASE_INSENSITIVE);
+            Matcher wordMatcher = wordPattern.matcher(lyrics);
+            List<Integer> wordIndices = new ArrayList<>();
+
+            while (wordMatcher.find()) {
+                wordIndices.add(wordMatcher.start()); // Store start index
             }
-            
+            matchers.add(wordIndices);
         }
-        
+
+//        lyrics = lyrics.toLowerCase().replaceAll("[^a-z ]", " ");
+//        lyricsPhrase = lyricsPhrase.toLowerCase().replaceAll("[^a-z ]", " ");
+//        String[] searchWords = lyricsPhrase.split("\\s+");
+//        String[] lyricsWords = lyrics.split("\\s+");
+//        
+//        int rank = 0;
+//        
+//        // Iterate through lyrics array until a word matches the first
+//        // the first word of the search phrase.
+//        
+//        int[] firstWordMatchLocs = new int[30];
+//        int j = 0;
+//        
+//        for(int i = 0; i < lyricsWords.length; i++) {
+//            if(lyricsWords[i].equals(searchWords[0])) {
+//                firstWordMatchLocs[j] = i;
+//                j++;
+//            }
+//        }
+//        
+//        StringBuilder fullLyricPhrase = new StringBuilder();
+//        
+//        for(int k = 0; k < firstWordMatchLocs.length; k++) {
+//            int l = firstWordMatchLocs[k];
+//            while(!lyricsWords[l].equals(searchWords[searchWords.length - 1])) {
+//                fullLyricPhrase.append(" ").append(lyricsWords[l]);
+//                l++;
+//            }
+//            
+//        }
+//        
+//        rank = fullLyricPhrase.toString().trim().length();
 //        for(int i = 0; i < lyricsWords.length; i++) {
 //            if(lyricsWords[i].equals(searchWords[0])) {
 //                fullLyricPhrase.append(lyricsWords[i]);
@@ -65,23 +88,21 @@ public class PhraseRanking {
 //                }
 //            }
 //        }
-
         //System.out.println(lyrics + ", " + lyricsPhrase);
-        if(rank == 0) {
-            return -1;
-        }
-        else {
-            return rank;
-        }
+//        if (rank == 0) {
+//            return -1;
+//        } else {
+//            return rank;
+//        }
+        return 0;
     }
-    
-    
+
     public static void main(String args[]) {
         SongCollection sc = new SongCollection(args[0]);
         int songCount = 0;
-        for(Song currSong : sc.getAllSongs()) {
+        for (Song currSong : sc.getAllSongs()) {
             int currSongRank = rankPhrase(currSong.getLyrics(), args[1]);
-            if(currSongRank > -1) {
+            if (currSongRank > -1) {
                 System.out.println(currSongRank + ", " + currSong.toString());
                 songCount++;
             }
